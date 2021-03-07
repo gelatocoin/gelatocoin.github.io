960297da51aaa6707b42e9f7ef3e03fd1a1aee48
Oke Kehinde <okekehinde@gmail.comm>
2021-03-07 09:38:52 +0100

    ui fixes

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
+
+const hamburger = document.querySelector(".hamburger")
+const menuItems = document.querySelector(".children")
+let showMenu = false;
+
+function togglemenu(){
+  if(!showMenu){
+    menuItems.classList.add("show")
+    return showMenu = true;
+  }
+    menuItems.classList.remove("show")
+    showMenu = false
+  
+}
+
+hamburger.addEventListener("click", togglemenu)
